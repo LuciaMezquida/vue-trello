4403859e50d5af4dd38f4ead143d37c2c1cae071
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-06-01 18:26:04 +0200

    Create TodoList.vue component

---
+++ TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <todo-item v-for="(todo, index) in todos" :key="index" :todo="todo"></todo-item>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  props: ['todos'],
+  methods: {}
+}
+</script>
+
+<style scoped> 
+  ul {
+    list-style-type: none;
+</style>
