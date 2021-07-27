8a8f51617cba9ab5618c55b238b5f16fa7cd2b17
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-27 18:26:39 +0200

    Create getters.js and actions.js and install firebase

--- vue-trello/src/store/index.js
+++ vue-trello/src/store/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
+import state from './state'
+import getters from './getters'
+import actions from './actions'
+import mutations from './mutations'
 
 Vue.use(Vuex) //así lo integramos como un plugin
 
